281124d60e5b646b3721e3073a70bebaf4fddc84
AmanVillar <51329512+AmanVillar@users.noreply.github.com>
2023-11-09 11:44:59 -0600

    Update ExpressionManager.h

--- ExpressionManager.h
+++ ExpressionManager.h
@@ -5,16 +5,16 @@
 
 class ExpressionManager {
 public:
-    // function to check if a character is an operator
+    //function to check if a character is an operator
     static bool isOperator(char c);
 
-    // function to determine the precedence of an operator
+    //function to determine the precedence of an operator
     static int getPrecedence(char op);
 
-    // function to check for balanced parentheses in an expression
+    //function to check for balanced parentheses in an expression
     static bool isBalancedParentheses(const std::string& expression);
 
-    // function to convert infix expression to postfix expression
+    //function to convert infix expression to postfix expression
     static std::string infixToPostfix(const std::string& infix);
 };
 
